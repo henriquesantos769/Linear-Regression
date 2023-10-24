8593d2d88b0a5e357a73e85601b461ef16914476
Main <luissavio150@gmail.com>
2023-10-24 10:54:50 -0300

    análise exploratória.

--- análise descritiva.py
+++ análise descritiva.py
@@ -1,31 +1,58 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Lasso
 
 df = pd.read_csv('Real-estate1.csv')
-'''
+print(df.info())
 print(df.head())
-print(df.describe())
-'''
+print(df['Y house price of unit area'].describe())
+
 # Histograma das variáveis
-x = df.drop(['No'],axis=1)
+x = df.drop(['No','X1 transaction date', 'X5 latitude','X6 longitude'],axis=1)
 x.hist(figsize=(12, 8))
 plt.show()
 
-# Matriz de correlação
-correlacao = df.corr()
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlacao, annot=True, cmap='coolwarm')
-plt.title('Matriz de Correlação')
+# Box plot para Y house price of unit area
+plt.figure(figsize=(8, 6))
+sns.boxplot(y=df['Y house price of unit area'])
+plt.title('Box Plot para Preço por Unidade de Área')
+plt.ylabel('Preço por Unidade de Área')
 plt.show()
 
-# Scatter plot de variáveis vs. preço
+# Gráfico de distribuição do preço das casas por unidade de área
+plt.figure(figsize=(12, 6))
+sns.histplot(df['Y house price of unit area'], kde=True)
+plt.title('Distribuição do Preço das Casas por Unidade de Área')
+plt.xlabel('Preço por Unidade de Área')
+plt.ylabel('Frequência')
+plt.show()
+
+# lasso para a verificação dos atributos mais importantes
+# Create X and y arrays
+X = df.drop(["Y house price of unit area"], axis=1)
+y = df["Y house price of unit area"].values
+sales_columns = X.columns
+
+# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.3)
+
+# Compute and print the coefficients
+lasso_coef = lasso.fit(X,y).coef_
+print(lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+plt.xticks(rotation=45)
+plt.show()
+
+# Scatter plot de idade da casa vs. preço
 plt.figure(figsize=(12, 6))
 sns.scatterplot(x='X2 house age', y='Y house price of unit area', data=df)
 plt.title('Scatter Plot: Idade da Casa vs. Preço')
 plt.show()
 
+# Scatter plot de distância até o metrô vs. preço
 plt.figure(figsize=(12, 6))
 sns.scatterplot(x='X3 distance to the nearest MRT station', y='Y house price of unit area', data=df)
 plt.title('Scatter Plot: Distância para a Estação MRT vs. Preço')
 plt.show()
+
